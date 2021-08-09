483ecefaadb0f15212a893fca46d970edfb1895a
Matthew Berginski <matthew.berginski@gmail.com>
2021-08-09 13:06:00 -0400

    updating to check for results file

--- src/build_ML_models_CCLE_data/send_rand_forest_cmd.R
+++ src/build_ML_models_CCLE_data/send_rand_forest_cmd.R
@@ -1,8 +1,16 @@
 #!/usr/bin/env Rscript
 
+library(here)
+num = 0
 for (hyper_num in 1:100) {
 	for (fold_num in 1:10) {
-		command = sprintf('sbatch --mem=32G --time=24:00:00 --wrap "./build_random_forest_models.R --hyper_slice %d --fold_number %d"', hyper_num,fold_num)
-		system(command)
+		target_file = sprintf('hyper%03d_fold%02d.rds',hyper_num,fold_num)
+		if (file.exists(here('results/rand_forest_klaeger_CCLE_models/',target_file))) {
+			next
+		} else {
+			command = sprintf('sbatch --mem=64G --time=24:00:00 --wrap "./build_random_forest_models.R --hyper_slice %d --fold_number %d"', hyper_num,fold_num)
+
+			system(command)
+		}
 	}
 }
