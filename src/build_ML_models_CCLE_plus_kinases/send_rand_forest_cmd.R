5ce9a29d8f0f9ffb7b1419872cfca0de8e51ac71
Matthew Berginski <matthew.berginski@gmail.com>
2021-08-23 10:17:03 -0400

    code to test models with all kinase expression data

---
+++ src/build_ML_models_CCLE_plus_kinases/send_rand_forest_cmd.R
@@ -0,0 +1,20 @@
+#!/usr/bin/env Rscript
+
+library(here)
+
+missing = 0
+for (hyper_num in seq(1,10)) {
+	for (fold_num in 1:10) {
+		target_file = sprintf('hyper%03d_fold%02d.rds',hyper_num,fold_num)
+		if (file.exists(here('results/single_model_plus_kinases/rand_forest_param_scan/',target_file))) {
+			next
+		} else {
+			missing = missing + 1
+			# command = sprintf('sbatch --mem=64G --time=24:00:00 --wrap "./build_random_forest_models.R --hyper_slice %d --fold_number %d"', hyper_num,fold_num)
+			local_command = sprintf('./build_random_forest_models.R --hyper_slice %d --fold_number %d', hyper_num,fold_num)
+			
+			# print(command)
+			system(local_command)
+		}
+	}
+}
