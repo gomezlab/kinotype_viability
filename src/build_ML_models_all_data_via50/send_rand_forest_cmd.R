1ac3be30f4f20ff3861f9b8f201bb61547bfb087
Matthew Berginski <matthew.berginski@gmail.com>
2021-09-07 11:41:03 -0400

    adding general rand forest testing models/submit script

--- src/build_ML_models_all_data_via50/send_rand_forest_cmd.R
+++ src/build_ML_models_all_data_via50/send_rand_forest_cmd.R
@@ -2,28 +2,14 @@
 
 library(here)
 
-missing = 0
-for (hyper_num in seq(1,10)) {
-	for (fold_num in 1:10) {
-		target_file = sprintf('hyper%03d_fold%02d.rds',hyper_num,fold_num)
+for (feature_num in c(200,300,400)) {
+	for (hyper_num in seq(1,10)) {
+		for (fold_num in 1:10) {
 		
-		missing = missing + 1
-		command = sprintf('sbatch --mem=64G --time=24:00:00 --wrap "./build_random_forest_models_500feat.R --hyper_slice %d --fold_number %d"', hyper_num,fold_num)
-		# command = sprintf('./build_random_forest_models.R --hyper_slice %d --fold_number %d', hyper_num,fold_num)
-		
-		# print(command)
-		system(command)
-		
-		command = sprintf('sbatch --mem=64G --time=24:00:00 --wrap "./build_random_forest_models_1000feat.R --hyper_slice %d --fold_number %d"', hyper_num,fold_num)
-		# command = sprintf('./build_random_forest_models.R --hyper_slice %d --fold_number %d', hyper_num,fold_num)
-		
-		# print(command)
-		system(command)
-		
-		command = sprintf('sbatch --mem=64G --time=24:00:00 --wrap "./build_random_forest_models_1500feat.R --hyper_slice %d --fold_number %d"', hyper_num,fold_num)
-		# command = sprintf('./build_random_forest_models.R --hyper_slice %d --fold_number %d', hyper_num,fold_num)
-		
-		# print(command)
-		system(command)
+			command = sprintf('sbatch --mem=64G --time=24:00:00 --wrap "./build_random_forest_models.R --feature_num %d --hyper_slice %d --fold_number %d"', feature_num, hyper_num, fold_num)	
+			
+			# print(command)
+			system(command)
+		}
 	}
 }
