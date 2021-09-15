ebc7f16a730dd8f2b46cfa4658535f6155372584
Matthew Berginski <matthew.berginski@gmail.com>
2021-09-15 11:15:55 -0400

    updates for new feat counts

--- src/build_ML_models_expression_regression/send_rand_forest_cmd.R
+++ src/build_ML_models_expression_regression/send_rand_forest_cmd.R
@@ -17,26 +17,28 @@ library(tidyverse)
 # 	}
 # }
 
-# for (feature_num in c(200,500,1000,1500)) {
-# 	for (hyper_num in 1:1) {
-# 		for (fold_num in 1:10) {
-# 
-# 			command = sprintf('sbatch --mem=64G --time=24:00:00 --wrap "./build_random_forest_models_no_tune.R --feature_num %d --hyper_slice %d --fold_number %d"', feature_num, hyper_num, fold_num)
-# 
-# 			# print(command)
-# 			system(command)
-# 		}
-# 	}
-# }
-
-for (feature_num in c(200,500,1000,1500)) {
+for (feature_num in c(300,400,2000)) {
 	for (hyper_num in 1:1) {
 		for (fold_num in 1:10) {
 			
-			command = sprintf('sbatch --mem=64G --job-name=xg_%d_%d --time=24:00:00 --wrap "./build_xgboost_models_no_tune.R --feature_num %d --hyper_slice %d --fold_number %d"',feature_num, fold_num, feature_num, hyper_num, fold_num)
-			
+			job_name = sprintf('RF_%d_%d',feature_num,fold_num)
+
+			command = sprintf('sbatch --job-name=%s --mem=64G --time=24:00:00 --wrap "./build_random_forest_models_no_tune.R --feature_num %d --hyper_slice %d --fold_number %d"', job_name, feature_num, hyper_num, fold_num)
+
 			# print(command)
 			system(command)
 		}
 	}
 }
+
+# for (feature_num in c(200,500,1000,1500)) {
+# 	for (hyper_num in 1:1) {
+# 		for (fold_num in 1:10) {
+# 			
+# 			command = sprintf('sbatch --mem=64G --job-name=xg_%d_%d --time=24:00:00 --wrap "./build_xgboost_models_no_tune.R --feature_num %d --hyper_slice %d --fold_number %d"',feature_num, fold_num, feature_num, hyper_num, fold_num)
+# 			
+# 			# print(command)
+# 			system(command)
+# 		}
+# 	}
+# }
