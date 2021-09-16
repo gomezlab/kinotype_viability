7d5072072f2f2cbc3f4f41fa45ffe2f17668158d
Matthew Berginski <matthew.berginski@gmail.com>
2021-09-16 14:43:50 -0400

    setting up for hyperparam scan on server

--- src/build_ML_models_expression_regression/send_rand_forest_cmd.R
+++ src/build_ML_models_expression_regression/send_rand_forest_cmd.R
@@ -3,33 +3,34 @@
 library(here)
 library(tidyverse)
 
-# rand_forest_grid = read_rds(here('results/single_model_all_data_regression/hyper_param_search_space.rds'))
-# 
-# for (feature_num in c(200,500,1000,1500)) {
-# 	for (hyper_num in 1:dim(rand_forest_grid)[1]) {
-# 		for (fold_num in 1:10) {
-# 
-# 			command = sprintf('sbatch --mem=64G --time=24:00:00 --wrap "./build_random_forest_models.R --feature_num %d --hyper_slice %d --fold_number %d"', feature_num, hyper_num, fold_num)
-# 
-# 			print(command)
-# 			# system(command)
-# 		}
-# 	}
-# }
+rand_forest_grid = read_rds(here('results/single_model_expression_regression/hyper_param_search_space.rds'))
 
-for (feature_num in c(300,400,2000)) {
-	for (hyper_num in 1:1) {
+for (feature_num in c(500,1000,1500)) {
+	for (hyper_num in 1:dim(rand_forest_grid)[1]) {
 		for (fold_num in 1:10) {
+			job_name = sprintf('RF_%d_%d_%d',feature_num,hyper_num,fold_num)
 			
-			job_name = sprintf('RF_%d_%d',feature_num,fold_num)
-
-			command = sprintf('sbatch --job-name=%s --mem=64G --time=24:00:00 --wrap "./build_random_forest_models_no_tune.R --feature_num %d --hyper_slice %d --fold_number %d"', job_name, feature_num, hyper_num, fold_num)
-
+			command = sprintf('sbatch --job-name=%s --mem=64G --time=24:00:00 --wrap "./build_random_forest_models.R --feature_num %d --hyper_slice %d --fold_number %d"', feature_num, hyper_num, fold_num)
+			
 			# print(command)
 			system(command)
 		}
 	}
 }
+
+# for (feature_num in c(300,400,2000)) {
+# 	for (hyper_num in 1:1) {
+# 		for (fold_num in 1:10) {
+# 			
+# 			job_name = sprintf('RF_%d_%d',feature_num,fold_num)
+# 
+# 			command = sprintf('sbatch --job-name=%s --mem=64G --time=24:00:00 --wrap "./build_random_forest_models_no_tune.R --feature_num %d --hyper_slice %d --fold_number %d"', job_name, feature_num, hyper_num, fold_num)
+# 
+# 			# print(command)
+# 			system(command)
+# 		}
+# 	}
+# }
 
 # for (feature_num in c(200,500,1000,1500)) {
 # 	for (hyper_num in 1:1) {
