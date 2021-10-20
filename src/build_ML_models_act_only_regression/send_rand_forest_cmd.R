503fcc0818d4ee561e20f3624e1a8e2e4b9760b1
Matthew Berginski <matthew.berginski@gmail.com>
2021-10-20 16:54:47 -0400

    code for running activation only regression models

---
+++ src/build_ML_models_act_only_regression/send_rand_forest_cmd.R
@@ -0,0 +1,46 @@
+#!/usr/bin/env Rscript
+
+library(here)
+library(tidyverse)
+library(glue)
+
+for (feature_num in c(50,100,150,200,250,300,350,400,450)) {
+	for (hyper_num in 1:dim(rand_forest_grid)[1]) {
+		for (fold_num in 1:10) {
+			job_name = sprintf('RF_%d_%d_%d',feature_num,hyper_num,fold_num)
+
+			command = sprintf('sbatch --job-name=%s --mem=32G --time=24:00:00 --wrap "./build_random_forest_models_no_tune.R --feature_num %d --hyper_slice %d --fold_number %d"', job_name, feature_num, hyper_num, fold_num)
+
+			# print(command)
+			system(command)
+		}
+	}
+}
+
+# system(paste0("parallel ::: '", glue_collapse(all_cmd_set, sep = "\' \'"), "'"))
+
+# for (feature_num in c(300,400,2000)) {
+# 	for (hyper_num in 1:1) {
+# 		for (fold_num in 1:10) {
+# 			
+# 			job_name = sprintf('RF_%d_%d',feature_num,fold_num)
+# 
+# 			command = sprintf('sbatch --job-name=%s --mem=64G --time=24:00:00 --wrap "./build_random_forest_models_no_tune.R --feature_num %d --hyper_slice %d --fold_number %d"', job_name, feature_num, hyper_num, fold_num)
+# 
+# 			# print(command)
+# 			system(command)
+# 		}
+# 	}
+# }
+
+# for (feature_num in c(200,500,1000,1500)) {
+# 	for (hyper_num in 1:1) {
+# 		for (fold_num in 1:10) {
+# 			
+# 			command = sprintf('sbatch --mem=64G --job-name=xg_%d_%d --time=24:00:00 --wrap "./build_xgboost_models_no_tune.R --feature_num %d --hyper_slice %d --fold_number %d"',feature_num, fold_num, feature_num, hyper_num, fold_num)
+# 			
+# 			# print(command)
+# 			system(command)
+# 		}
+# 	}
+# }
