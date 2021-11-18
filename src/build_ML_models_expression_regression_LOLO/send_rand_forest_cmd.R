44191639d2552eb3563a6765a41af16d427be0da
Matthew Berginski <matthew.berginski@gmail.com>
2021-11-18 14:04:02 -0500

    adding code for model testing and sending commands

---
+++ src/build_ML_models_expression_regression_LOLO/send_rand_forest_cmd.R
@@ -0,0 +1,32 @@
+#!/usr/bin/env Rscript
+
+library(here)
+library(tidyverse)
+
+rand_forest_grid = read_rds(here('results/single_model_expression_regression/hyper_param_search_space.rds'))
+
+
+for (feature_num in c(1500)) {
+	for (fold_num in 1:476) {
+		
+		job_name = sprintf('RF_%d_%04d',feature_num,fold_num)
+		
+		command = sprintf('sbatch --job-name=%s --mem=45G -c 8 --time=7-00:00:00 --wrap "./build_random_forest_models_no_tune.R --feature_num %d --fold_number %d"', job_name, feature_num, fold_num)
+		
+		print(command)
+		# system(command)
+	}
+	
+}
+
+# for (feature_num in c(200,500,1000,1500)) {
+# 	for (hyper_num in 1:1) {
+# 		for (fold_num in 1:10) {
+# 			
+# 			command = sprintf('sbatch --mem=64G --job-name=xg_%d_%d --time=24:00:00 --wrap "./build_xgboost_models_no_tune.R --feature_num %d --hyper_slice %d --fold_number %d"',feature_num, fold_num, feature_num, hyper_num, fold_num)
+# 			
+# 			# print(command)
+# 			system(command)
+# 		}
+# 	}
+# }
