6866d2d3027ea6336108054938638f58f3cee85b
Matthew Berginski <matthew.berginski@gmail.com>
2021-11-22 13:33:29 -0500

    adding code for testing 1.2 truncated PRISM model

---
+++ src/build_ML_models_expression_regression_1.2_trunc/send_rand_forest_cmd.R
@@ -0,0 +1,19 @@
+#!/usr/bin/env Rscript
+
+library(here)
+library(tidyverse)
+
+fold_ids = read_rds(here('results/single_model_expression_regression_1.2_trunc/CV_split_row_nums.rds'))
+
+for (feature_num in c(1500)) {
+	for (fold_num in unique(fold_ids)) {
+		
+		job_name = sprintf('RF_%d_%04d',feature_num,fold_num)
+		
+		command = sprintf('sbatch --job-name=%s --mem=64G -c 8 --time=7-00:00:00 --wrap "./build_random_forest_models_no_tune.R --feature_num %d --CV_fold_ID %d"', job_name, feature_num, fold_num)
+		
+		# print(command)
+		system(command)
+	}
+	
+}
