cda89c078379d89d6746bcd7b6ddea9797c382d5
Matthew Berginski <matthew.berginski@gmail.com>
2021-12-03 11:00:20 -0500

    updates for questions from Jim

--- src/misc/cyto_messing_around.R
+++ src/misc/cyto_messing_around.R
@@ -1,3 +1,6 @@
+library(here)
+library(tidyverse)
+
 klaeger_full_tidy %>% 
 	filter(gene_name == "ACTR3", relative_intensity != 1) %>% 
 	ggplot(aes(x=log10(concentration_M),y=relative_intensity,color=drug)) + 
@@ -12,8 +15,28 @@ BerginskiRMisc::trimImage(here('arp3_kin_inhib_pulldown.png'))
 cob_temp = klaeger_full_tidy %>% 
 	filter(drug == "Cobimetinib", relative_intensity != 1)
 
+klaeger_full_tidy %>% 
+	filter(drug == "Cobimetinib", relative_intensity != 1) %>% 
+	ggplot(aes(x=log10(concentration_M),y=relative_intensity, color=gene_name)) + 
+	geom_hline(aes(yintercept=1), linetype = 2, alpha = 0.5) +	
+	geom_line() +
+	geom_point() +
+	BerginskiRMisc::theme_berginski()
+
+ggsave(here('Cobimetinib_hits.png'))
+
 gil_temp = klaeger_full_tidy %>% 
 	filter(drug == "Gilteritinib", relative_intensity != 1)
+
+klaeger_full_tidy %>% 
+	filter(drug == "Gilteritinib", relative_intensity != 1) %>% 
+	ggplot(aes(x=log10(concentration_M),y=relative_intensity, color=gene_name)) + 
+	geom_hline(aes(yintercept=1), linetype = 2, alpha = 0.5) +	
+	geom_line() +
+	geom_point() +
+	BerginskiRMisc::theme_berginski()
+
+ggsave(here('Gilteritinib_hits.png'))
 
 klaeger_full_tidy %>% 
 	filter(gene_name == "ACTR2", relative_intensity != 1) %>% 
@@ -29,8 +52,47 @@ BerginskiRMisc::trimImage(here('arp2_kin_inhib_pulldown.png'))
 tal_temp = klaeger_full_tidy %>% 
 	filter(drug == "Talmapimod", relative_intensity != 1)
 
+klaeger_full_tidy %>% 
+	filter(drug == "Talmapimod", relative_intensity != 1) %>% 
+	ggplot(aes(x=log10(concentration_M),y=relative_intensity, color=gene_name)) + 
+	geom_hline(aes(yintercept=1), linetype = 2, alpha = 0.5) +	
+	geom_line() +
+	geom_point() +
+	BerginskiRMisc::theme_berginski()
+ggsave(here('Talmapimod_hits.png'))
+
 cyto_hits = data.frame(gene_names = c("ACTR2","ACTR3","ADD2","CAPZA1","CKAP5","CTTN","DYNLL1","FLNB","MYH10","MYH14","PRPH","OSBPL3")) %>%
 	mutate("act_str" = paste0("act_",gene_names))
 
-# importance = as.data.frame(final_model_1500feat$fit$fit$fit$variable.importance) %>% rownames_to_column() %>%
-# 	filter(rowname %in% cyto_hits$act_str)
+klaeger_full_tidy %>% 
+	filter(drug == "Selumetinib", relative_intensity != 1) %>% 
+	ggplot(aes(x=log10(concentration_M),y=relative_intensity, color=gene_name)) + 
+	geom_hline(aes(yintercept=1), linetype = 2, alpha = 0.5) +	
+	geom_line() +
+	geom_point() +
+	BerginskiRMisc::theme_berginski()
+
+ggsave(here('Selumetinib_hits.png'))
+
+###############################################################################
+TNK2_hits = klaeger_full_tidy %>% 
+	filter(gene_name == "TNK2", relative_intensity != 1)
+
+strong_TNK2 = klaeger_full_tidy %>% 
+	filter(gene_name == "TNK2", relative_intensity == 0)
+
+klaeger_full_tidy %>% 
+	filter(drug == "ASP-3026", relative_intensity != 1) %>% 
+	ggplot(aes(x=log10(concentration_M),y=relative_intensity, color=gene_name)) + 
+	geom_hline(aes(yintercept=1), linetype = 2, alpha = 0.5) +	
+	geom_line() +
+	geom_point() +
+	BerginskiRMisc::theme_berginski()
+
+ggsave(here('ASP-3026_hits.png'))
+
+asp_hits = klaeger_full_tidy %>%
+	filter(drug == "ASP-3026", relative_intensity != 1)
+
+tofact_hits = klaeger_full_tidy %>%
+	filter(drug == "Tofacitinib", relative_intensity != 1)
