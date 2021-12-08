3d42ea17404d0609b52090afe852b6d172cb198b
Matthew Berginski <matthew.berginski@gmail.com>
2021-12-08 14:44:17 -0500

    files for testing cluster feature selection

---
+++ src/build_ML_models_expression_regression_cluster/get_feat_cor.R
@@ -0,0 +1,82 @@
+#!/usr/bin/env Rscript
+
+library(tidyverse)
+library(here)
+library(tidymodels)
+library(tictoc)
+library(doParallel)
+library(patchwork)
+library(argparse)
+
+tic()
+
+###############################################################################
+# Parse Parameters/Check for Fold IDs file
+###############################################################################
+
+parser <- ArgumentParser(description='Process input paramters')
+parser$add_argument('--CV_fold_ID', default = 1, type="integer")
+
+args = parser$parse_args()
+
+dir.create(here('results/single_model_expression_regression_cluster'), recursive = T)
+
+source(here('src/build_ML_models_expression_regression/shared_feature_selection_functions.r'))
+
+###############################################################################
+# Load Data
+###############################################################################
+
+klaeger_wide = read_rds(here('results/klaeger_full_tidy.rds')) %>%
+	mutate(act_gene_name = paste0("act_",gene_name)) %>%
+	select(-gene_name) %>%
+	pivot_wider(names_from = act_gene_name, values_from = relative_intensity)
+
+PRISM_klaeger_imputed = read_rds(here('results/PRISM_klaeger_imputed_tidy.rds'))
+
+CCLE_data = read_rds(here('results/single_model/full_CCLE_expression_set_for_ML.rds'))
+
+PRISM_klaeger_imputed = PRISM_klaeger_imputed %>%
+	filter(depmap_id %in% CCLE_data$DepMap_ID) %>%
+	ungroup()
+
+if (file.exists(here('results/single_model_expression_regression_cluster/CV_split_row_nums.rds'))) {
+	fold_ids = read_rds(here('results/single_model_expression_regression_cluster/CV_split_row_nums.rds'))
+} else {
+	combo_id_nums = PRISM_klaeger_imputed %>%
+		select(depmap_id,drug) %>%
+		unique() %>%
+		mutate(id_num = sample(rep(1:10,length.out = n())))
+	
+	fold_ids = PRISM_klaeger_imputed %>%
+		left_join(combo_id_nums) %>% 
+		pull(id_num)
+	
+	write_rds(fold_ids, here('results/single_model_expression_regression_cluster/CV_split_row_nums.rds'))
+}
+
+stopifnot(args$CV_fold_ID <= max(fold_ids))
+
+###############################################################################
+# Output Full Data Set for Final Model Building
+###############################################################################
+
+# if (! file.exists(here('results/single_model_expression_regression_cluster/full_model_data_set_500feat.rds'))) {
+# 	all_cor = find_feature_correlations()
+# 	write_rds(all_cor,here('results/single_model_expression_regression_cluster/full_data_cor.rds'))
+# 	build_regression_viability_set(all_cor,500) %>%
+# 		write_rds(here('results/single_model_expression_regression_cluster/full_model_data_set_500feat.rds'), compress='gz')
+# }
+
+###############################################################################
+# Calc Feature Cor and Output
+###############################################################################
+
+feature_cor = find_feature_correlations(row_indexes = which(fold_ids != args$CV_fold_ID))
+
+dir.create(here('results/single_model_expression_regression_cluster/CV_feature_cors/'), recursive = T, showWarnings = F)
+
+write_rds(feature_cor,
+					here('results/single_model_expression_regression_cluster/CV_feature_cors/',sprintf('%04d.rds',args$CV_fold_ID)),
+					compress = 'gz')
+toc()
--- src/build_ML_models_expression_regression_cluster/get_feat_set_cluster_rep.R
+++ src/build_ML_models_expression_regression_cluster/get_feat_set_cluster_rep.R
@@ -20,9 +20,17 @@ parser$add_argument('--CV_fold_ID', default = 1, type="integer")
 
 args = parser$parse_args()
 
+dir.create(here('results/single_model_expression_regression_cluster_cluster/cluster_sets'),
+					 showWarnings = F, recursive = T)
+
+cluster_file = here('results/single_model_expression_regression_cluster_cluster/CV_cluster_sets',
+											sprintf('%04d.rds', args$CV_fold_ID))
+
 ###############################################################################
 # Load Data
 ###############################################################################
+
+fold_ids = read_rds(here('results/single_model_expression_regression_cluster/CV_split_row_nums.rds'))
 
 klaeger_wide = read_rds(here('results/klaeger_full_tidy.rds')) %>%
 	mutate(act_gene_name = paste0("act_",gene_name)) %>%
@@ -37,42 +45,24 @@ PRISM_klaeger_imputed = PRISM_klaeger_imputed %>%
 	filter(depmap_id %in% CCLE_data$DepMap_ID) %>%
 	ungroup()
 
-temp = PRISM_klaeger_imputed %>% 
-	# slice_sample(prop=0.75) %>%
+###############################################################################
+# Combine Data and 
+###############################################################################
+
+combined_data = PRISM_klaeger_imputed %>% 
+	slice(which(fold_ids != args$CV_fold_ID)) %>%
 	left_join(klaeger_wide, by = c('drug'='drug','klaeger_conc'='concentration_M')) %>% 
 	left_join(CCLE_data, by=c('depmap_id'='DepMap_ID')) %>%
 	mutate(target_viability = imputed_viability)
 
-# cross_cor_mat = HiClimR::fastCor(temp %>% select(starts_with("act_"),starts_with("exp_")), optBLAS = T, nSplit = 1000, upperTri = T)
-# 
-# write_rds(cross_cor_mat,here('src/misc/cross_cor_mat.rds'), compres = 'gz')
+sd_vals = apply(combined_data,2,sd)
 
-# if (file.exists(here('results/single_model_expression_regression_combo_10fold/CV_split_row_nums.rds'))) {
-# 	fold_ids = read_rds(here('results/single_model_expression_regression_combo_10fold/CV_split_row_nums.rds'))
-# } else {
-# 	combo_id_nums = PRISM_klaeger_imputed %>%
-# 		select(depmap_id,drug) %>%
-# 		unique() %>%
-# 		mutate(id_num = sample(rep(1:10,length.out = n())))
-# 	
-# 	fold_ids = PRISM_klaeger_imputed %>%
-# 		left_join(combo_id_nums) %>% 
-# 		pull(id_num)
-# 	
-# 	write_rds(fold_ids, here('results/single_model_expression_regression_combo_10fold/CV_split_row_nums.rds'))
-# }
+combined_data = combined_data %>%
+	select(-any_of(names(which(sd_vals == 0))))
 
-# stopifnot(args$CV_fold_ID <= max(fold_ids))
+cross_cor_mat = 1 - HiClimR::fastCor(combined_data %>% select(starts_with("act_"),starts_with("exp_")), 
+																		 optBLAS = T, nSplit = 100, upperTri = T)
 
-# ###############################################################################
-# # Calc Feature Cor and Output
-# ###############################################################################
-# 
-# feature_cor = find_feature_correlations(row_indexes = which(fold_ids != args$CV_fold_ID))
-# 
-# dir.create(here('results/single_model_expression_regression_combo_10fold/CV_feature_cors/'), recursive = T, showWarnings = F)
-# 
-# write_rds(feature_cor,
-# 					here('results/single_model_expression_regression_combo_10fold/CV_feature_cors/',sprintf('%04d.rds',args$CV_fold_ID)),
-# 					compress = 'gz')
-# toc()
+write_rds(hclust(as.dist(cross_cor_mat)), cluster_file)
+
+toc()
