53bfbad439db2fcb59bfc792a89acfb899c3a2e9
Matthew Berginski <matthew.berginski@gmail.com>
2021-12-10 11:32:56 -0500

    rand forest tuning code/update to send command

--- src/build_ML_models_expression_regression/build_random_forest_models.R
+++ src/build_ML_models_expression_regression/build_random_forest_models.R
@@ -9,46 +9,87 @@ library(argparse)
 
 tic()
 
-print(commandArgs())
-
 parser <- ArgumentParser(description='Process input paramters')
-parser$add_argument('--feature_num', default = 500, type="integer")
-parser$add_argument('--hyper_slice', default = 1, type="integer")
-parser$add_argument('--fold_number', default = 1, type="integer")
-
+parser$add_argument('--feature_num', default = 100, type="integer")
+parser$add_argument('--CV_fold_ID', default = 1, type="integer")
+parser$add_argument('--trees', default = 1000, type="integer")
 
 args = parser$parse_args()
-print(sprintf('Hyper: %03d, Fold: %02d',args$hyper_slice,args$fold_number))
+print(sprintf('Fold: %02d',args$CV_fold_ID))
 
 dir.create(here('results/single_model_expression_regression/', 
-								sprintf('rand_forest_param_scan_%dfeat/',args$feature_num)), showWarnings = F)
+								sprintf('rand_forest_tune/%dfeat_%dtrees/',args$feature_num,args$trees)), 
+					 showWarnings = F, recursive = T)
 
-output_file = here('results/single_model_expression_regression/', 
-									 sprintf('rand_forest_param_scan_%dfeat/',args$feature_num),
-									 sprintf('hyper%03d_fold%02d.rds',args$hyper_slice,args$fold_number))
+full_output_file = here('results/single_model_expression_regression/', 
+												sprintf('rand_forest_tune/%dfeat_%dtrees/',args$feature_num,args$trees),
+												sprintf('fold%04d_test.rds',args$CV_fold_ID))
 
-binarized_viability_CV = read_rds(here('results/single_model_expression_regression',
-																			 sprintf('CV_splits_%dfeat/',args$feature_num),
-																			 sprintf('%02d.rds',args$fold_number)))
+dir.create(here('results/single_model_expression_regression/', 
+								sprintf('rand_forest_tune/%dfeat_%dtrees_pred/',args$feature_num,args$trees)), showWarnings = F)
 
-rand_forest_grid = read_rds(here('results/single_model_expression_regression/hyper_param_search_space.rds')) %>% 
-														slice(args$hyper_slice)
-print(rand_forest_grid)
+pred_output_file = here('results/single_model_expression_regression/', 
+												sprintf('rand_forest_tune/%dfeat_%dtrees_pred/',args$feature_num,args$trees),
+												sprintf('fold%04d_test.rds',args$CV_fold_ID))
+
+feature_cor = read_rds(here('results/single_model_expression_regression/CV_feature_cors/',
+														sprintf('%04d.rds',args$CV_fold_ID)))
+
+fold_ids = read_rds(here('results/single_model_expression_regression/CV_split_row_nums.rds'))
+
+source(here('src/build_ML_models_expression_regression/shared_feature_selection_functions.r'))
 
 ###############################################################################
-# Build Models
+# Load Data
 ###############################################################################
 
-PRISM_klaeger_recipe = recipe(target_viability ~ ., binarized_viability_CV$splits[[1]]$data) %>%
+klaeger_wide = read_rds(here('results/klaeger_full_tidy.rds')) %>%
+	mutate(act_gene_name = paste0("act_",gene_name)) %>%
+	select(-gene_name) %>%
+	pivot_wider(names_from = act_gene_name, values_from = relative_intensity)
+
+PRISM_klaeger_imputed = read_rds(here('results/PRISM_klaeger_imputed_tidy.rds'))
+
+CCLE_data = read_rds(here('results/single_model/full_CCLE_expression_set_for_ML.rds'))
+
+PRISM_klaeger_imputed = PRISM_klaeger_imputed %>%
+	filter(depmap_id %in% CCLE_data$DepMap_ID) %>%
+	ungroup()
+
+###############################################################################
+# Setup and Run Model
+###############################################################################
+
+###########################################################
+# Build Cross Validation Data Set
+###########################################################
+splits = list()
+
+splits[[1]] = make_splits(list("analysis" = which(fold_ids != args$CV_fold_ID),
+															 "assessment" = which(fold_ids == args$CV_fold_ID)),
+													build_regression_viability_set(feature_cor,args$feature_num))
+
+id = sprintf("Fold%02d",args$CV_fold_ID)
+
+cross_validation_set = new_rset(
+	splits = splits,
+	ids = id,
+	attrib = sprintf("Per compound cv splits for fold ", args$CV_fold_ID),
+	subclass = c("vfold_cv", "rset")
+)
+
+###########################################################
+# Run Model
+###########################################################
+PRISM_klaeger_recipe = recipe(target_viability ~ ., cross_validation_set$splits[[1]]$data) %>%
 	update_role(-starts_with("act_"),
 							-starts_with("exp_"),
 							-starts_with("dep_"),
 							-starts_with("target_"), new_role = "id variable") %>%
 	prep()
 
-rand_forest_spec <- rand_forest(
-	trees = tune()
-) %>% set_engine("ranger") %>%
+rand_forest_spec <- rand_forest(trees = args$trees) %>% 
+	set_engine("ranger", num.threads = 8) %>%
 	set_mode("regression")
 
 rand_forest_wf <- workflow() %>%
@@ -57,9 +98,10 @@ rand_forest_wf <- workflow() %>%
 
 model_results <- tune_grid(
 	rand_forest_wf,
-	resamples = binarized_viability_CV,
-	grid = rand_forest_grid,
+	resamples = cross_validation_set,
 	control = control_grid(save_pred = TRUE)
-) %>% write_rds(output_file, compress = 'gz')
+) %>% write_rds(full_output_file, compress = 'gz')
+
+write_rds(model_results$.predictions[[1]], pred_output_file, compress = 'gz')
 
 toc()
--- src/build_ML_models_expression_regression/send_model_cmd.R
+++ src/build_ML_models_expression_regression/send_model_cmd.R
@@ -17,6 +17,20 @@ for (feature_num in c(100,200,300,400,500,1000,1500,2000)) {
 	}
 }
 
+for (feature_num in c(500,1000,1500,2000)) {
+	for (fold_num in unique(fold_ids)) {
+		for (trees in c(1000,1500,2000)) {
+			
+			job_name = sprintf('RF_%d_%04d_%dt',feature_num,fold_num,trees)
+			
+			command = sprintf('sbatch --job-name=%s --mem=64G -c 8 --time=12:00:00 --wrap "./build_random_forest_models.R --feature_num %d --CV_fold_ID %d --trees %d"', job_name, feature_num, fold_num, trees)
+			
+			# print(command)
+			system(command)
+		}
+	}
+}
+
 for (feature_num in c(100,200,300,400,500,1000,1500,2000)) {
 	for (fold_num in unique(fold_ids)) {
 		
