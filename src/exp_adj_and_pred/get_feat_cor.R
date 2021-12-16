639d319e1b2483aebe16e5cf9041b544c022f097
Matthew Berginski <matthew.berginski@gmail.com>
2021-12-16 14:42:27 -0500

    files for implementing Exp adjusted act vector testing

---
+++ src/exp_adj_and_pred/get_feat_cor.R
@@ -0,0 +1,79 @@
+#!/usr/bin/env Rscript
+
+library(tidyverse)
+library(here)
+library(tidymodels)
+library(tictoc)
+library(doParallel)
+library(patchwork)
+library(argparse)
+
+tic()
+
+###############################################################################
+# Parse Parameters/Check for Fold IDs file
+###############################################################################
+
+parser <- ArgumentParser(description='Process input paramters')
+parser$add_argument('--CV_fold_ID', default = 1, type="integer")
+
+args = parser$parse_args()
+
+dir.create(here('results/exp_adj'), recursive = T)
+
+source(here('src/exp_adj_and_pred/shared_feature_selection_functions.r'))
+
+###############################################################################
+# Load Data
+###############################################################################
+
+PRISM_klaeger_imputed = read_rds(here('results/PRISM_klaeger_imputed_tidy.rds'))
+
+klaeger_wide = read_rds(here('results/exp_adj/klaeger_wide_below_10_per_gene.rds'))
+
+CCLE_data = read_rds(here('results/single_model/full_CCLE_expression_set_for_ML.rds'))
+
+PRISM_klaeger_imputed = PRISM_klaeger_imputed %>%
+	filter(depmap_id %in% CCLE_data$DepMap_ID) %>%
+	ungroup()
+
+if (file.exists(here('results/exp_adj/CV_split_row_nums.rds'))) {
+	fold_ids = read_rds(here('results/exp_adj/CV_split_row_nums.rds'))
+} else {
+	combo_id_nums = PRISM_klaeger_imputed %>%
+		select(depmap_id,drug) %>%
+		unique() %>%
+		mutate(id_num = sample(rep(1:10,length.out = n())))
+
+	fold_ids = PRISM_klaeger_imputed %>%
+		left_join(combo_id_nums) %>%
+		pull(id_num)
+
+	write_rds(fold_ids, here('results/exp_adj/CV_split_row_nums.rds'))
+}
+
+stopifnot(args$CV_fold_ID <= max(fold_ids))
+
+###############################################################################
+# Output Full Data Set for Final Model Building
+###############################################################################
+
+# if (! file.exists(here('results/exp_adj/full_model_data_set_500feat.rds'))) {
+# 	all_cor = find_feature_correlations()
+# 	write_rds(all_cor,here('results/exp_adj/full_data_cor.rds'))
+# 	build_regression_viability_set(all_cor,500) %>%
+# 		write_rds(here('results/exp_adj/full_model_data_set_500feat.rds'), compress='gz')
+# }
+
+###############################################################################
+# Calc Feature Cor and Output
+###############################################################################
+
+feature_cor = find_feature_correlations(row_indexes = which(fold_ids != args$CV_fold_ID))
+
+dir.create(here('results/exp_adj/CV_feature_cors/'), recursive = T, showWarnings = F)
+
+write_rds(feature_cor,
+					here('results/exp_adj/CV_feature_cors/',sprintf('%04d.rds',args$CV_fold_ID)),
+					compress = 'gz')
+toc()
