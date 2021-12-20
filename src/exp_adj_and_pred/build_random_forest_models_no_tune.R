4103a9b0711a7c568072d6c7fdc760d6b3cf3a3f
Matthew Berginski <matthew.berginski@gmail.com>
2021-12-20 13:34:18 -0500

    changing out for below 20 exp adj

--- src/exp_adj_and_pred/build_random_forest_models_no_tune.R
+++ src/exp_adj_and_pred/build_random_forest_models_no_tune.R
@@ -44,7 +44,7 @@ source(here('src/exp_adj_and_pred/shared_feature_selection_functions.r'))
 
 PRISM_klaeger_imputed = read_rds(here('results/PRISM_klaeger_imputed_tidy.rds'))
 
-klaeger_wide = read_rds(here('results/exp_adj/klaeger_wide_zero_adj.rds'))
+klaeger_wide = read_rds(here('results/exp_adj/klaeger_wide_below_20_per_gene.rds'))
 
 CCLE_data = read_rds(here('results/single_model/full_CCLE_expression_set_for_ML.rds'))
 
