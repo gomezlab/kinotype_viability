91ef761f73d329cd31da490b801f83c66205a261
Matthew Berginski <matthew.berginski@gmail.com>
2022-03-16 14:11:05 -0400

    adding script for making the zip file for zenodo upload

---
+++ make_zip_for_zenodo.sh
@@ -0,0 +1,3 @@
+zip -r zenodo_data.zip data/
+
+zip -r zenodo_results.zip results/single_model results/single_model_expression_regression results/single_model_all_data_regression results/validation_results results/single_model_exp_only_all_data_lines results/*.rds
