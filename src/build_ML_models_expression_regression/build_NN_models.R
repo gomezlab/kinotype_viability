9c0863bba8b6b16bcfa31029e1c4bb0d4c2c5933
Matthew Berginski <matthew.berginski@gmail.com>
2022-08-03 12:30:16 -0400

    updating NN model code to use tabnet

--- src/build_ML_models_expression_regression/build_NN_models.R
+++ src/build_ML_models_expression_regression/build_NN_models.R
@@ -6,59 +6,102 @@ library(tidymodels)
 library(tictoc)
 library(doParallel)
 library(argparse)
+library(tabnet)
 
 tic()
 
-print(commandArgs())
-
 parser <- ArgumentParser(description='Process input paramters')
-parser$add_argument('--feature_num', default = 500, type="integer")
-parser$add_argument('--hyper_slice', default = 1, type="integer")
-parser$add_argument('--fold_number', default = 1, type="integer")
+parser$add_argument('--feature_num', default = 100, type="integer")
+parser$add_argument('--CV_fold_ID', default = 1, type="integer")
 
 args = parser$parse_args()
-print(sprintf('Hyper: %03d, Fold: %02d',args$hyper_slice,args$fold_number))
+print(sprintf('Fold: %02d',args$CV_fold_ID))
 
 dir.create(here('results/single_model_expression_regression/', 
-								sprintf('NN_param_scan_%dfeat/',args$feature_num)), showWarnings = F)
+								sprintf('tabnet/%dfeat_notune/',args$feature_num)), 
+					 showWarnings = F, recursive = T)
 
-output_file = here('results/single_model_expression_regression/', 
-									 sprintf('NN_param_scan_%dfeat/',args$feature_num),
-									 sprintf('hyper%03d_fold%02d.rds',args$hyper_slice,args$fold_number))
+full_output_file = here('results/single_model_expression_regression/', 
+												sprintf('tabnet/%dfeat_notune/',args$feature_num),
+												sprintf('fold%04d_test.rds',args$CV_fold_ID))
 
-binarized_viability_CV = read_rds(here('results/single_model_expression_regression',
-																			 sprintf('CV_splits_%dfeat/',args$feature_num),
-																			 sprintf('%02d.rds',args$fold_number)))
+dir.create(here('results/single_model_expression_regression/', 
+								sprintf('tabnet/%dfeat_notune_pred/',args$feature_num)), showWarnings = F)
+
+pred_output_file = here('results/single_model_expression_regression/', 
+												sprintf('tabnet/%dfeat_notune_pred/',args$feature_num),
+												sprintf('fold%04d_test.rds',args$CV_fold_ID))
+
+feature_cor = read_rds(here('results/single_model_expression_regression/CV_feature_cors/',
+														sprintf('%04d.rds',args$CV_fold_ID)))
+
+fold_ids = read_rds(here('results/single_model_expression_regression/CV_split_row_nums.rds'))
+
+source(here('src/build_ML_models_expression_regression/shared_feature_selection_functions.r'))
 
 ###############################################################################
-# Build Models
+# Load Data
 ###############################################################################
 
-PRISM_klaeger_recipe = recipe(target_viability ~ ., binarized_viability_CV$splits[[1]]$data) %>%
+klaeger_wide = read_rds(here('results/klaeger_full_tidy.rds')) %>%
+	mutate(act_gene_name = paste0("act_",gene_name)) %>%
+	select(-gene_name) %>%
+	pivot_wider(names_from = act_gene_name, values_from = relative_intensity)
+
+PRISM_klaeger_imputed = read_rds(here('results/PRISM_klaeger_imputed_tidy.rds'))
+
+CCLE_data = read_rds(here('results/single_model/full_CCLE_expression_set_for_ML.rds'))
+
+PRISM_klaeger_imputed = PRISM_klaeger_imputed %>%
+	filter(depmap_id %in% CCLE_data$DepMap_ID) %>%
+	ungroup()
+
+###############################################################################
+# Setup and Run Model
+###############################################################################
+
+###########################################################
+# Build Cross Validation Data Set
+###########################################################
+splits = list()
+
+splits[[1]] = make_splits(list("analysis" = which(fold_ids != args$CV_fold_ID),
+															 "assessment" = which(fold_ids == args$CV_fold_ID)),
+													build_regression_viability_set(feature_cor,args$feature_num))
+
+id = sprintf("Fold%02d",args$CV_fold_ID)
+
+cross_validation_set = new_rset(
+	splits = splits,
+	ids = id,
+	attrib = sprintf("Per compound cv splits for fold ", args$CV_fold_ID),
+	subclass = c("vfold_cv", "rset")
+)
+
+###########################################################
+# Run Model
+###########################################################
+PRISM_klaeger_recipe = recipe(target_viability ~ ., cross_validation_set$splits[[1]]$data) %>%
 	update_role(-starts_with("act_"),
 							-starts_with("exp_"),
 							-starts_with("dep_"),
 							-starts_with("target_"), new_role = "id variable") %>%
-	step_BoxCox(all_predictors())%>%
-	step_normalize(all_predictors()) %>%
 	prep()
 
-library(keras)
-reticulate::use_condaenv('tf-gpu',required =T)
+tabnet_spec <- tabnet() %>% 
+	set_engine("torch") %>%
+	set_mode("regression")
 
-NN_spec <- mlp(epochs = 100, hidden_units = 5, dropout = 0.1) %>%
-	set_mode("regression") %>% 
-	# Also set engine-specific `verbose` argument to prevent logging the results: 
-	set_engine("keras", verbose = 0)
-
-NN_wf <- workflow() %>%
-	add_model(NN_spec) %>%
+tabnet_wf <- workflow() %>%
+	add_model(tabnet_spec) %>%
 	add_recipe(PRISM_klaeger_recipe)
 
-# model_results <- tune_grid(
-# 	NN_wf,
-# 	resamples = binarized_viability_CV,
-# 	control = control_grid(save_pred = TRUE)
-# ) %>% write_rds(output_file, compress = 'gz')
-notifysendR::notify_send("done")
+model_results <- tune_grid(
+	tabnet_wf,
+	resamples = cross_validation_set,
+	control = control_grid(save_pred = TRUE)
+) %>% write_rds(full_output_file, compress = 'gz')
+
+write_rds(model_results$.predictions[[1]], pred_output_file, compress = 'gz')
+
 toc()
